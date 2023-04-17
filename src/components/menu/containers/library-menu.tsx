d8a1fb10d42b8096157015c257d60632f434a0fc
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-04-17 12:02:39 -0300

    Update library-menu.tsx

--- src/components/menu/containers/library-menu.tsx
+++ src/components/menu/containers/library-menu.tsx
@@ -4,7 +4,6 @@ import {
   MenuItemDescription,
   ButtonIcon,
   MenuContainer,
-  Link
 } from "../menu-style";
 import History from '../../../assets/historia.png'
 import Videos from '../../../assets/video.png'
@@ -13,11 +12,11 @@ import Like from '../../../assets/gostar.png'
 import Play from '../../../assets/botao-play.png'
 
 const items = [
-  {icon: History, description: 'Histórico', link: '/historico'},
-  {icon: Videos, description: 'Seus vídeos', link: '/yourvideos'},
-  {icon: Watch, description: 'Assistir mais tarde', link: '/assistir-mais-tarde'},
-  {icon: Like, description: 'Vídeos marcados como "Gostei"', link: '/like'},
-  {icon: Play, description: 'Mix - Cd completo gospel', link: '/mix'}
+  {icon: History, description: 'Histórico'},
+  {icon: Videos, description: 'Seus vídeos'},
+  {icon: Watch, description: 'Assistir mais tarde'},
+  {icon: Like, description: 'Vídeos marcados como "Gostei"'},
+  {icon: Play, description: 'Mix - Cd completo gospel'}
 ]
 
 function LibraryMenu() {
@@ -27,12 +26,10 @@ function LibraryMenu() {
   return (
     <MenuContainer>
       {items.map((item, index) => (
-        <Link to={item.link}>
-          <MenuItem key={index} openMenu={openMenu}>
-            <ButtonIcon alt="Ícone" src={item.icon}/>
-            <MenuItemDescription openMenu={openMenu}>{item.description}</MenuItemDescription>
-          </MenuItem>
-        </Link>
+        <MenuItem key={index} openMenu={openMenu}>
+          <ButtonIcon alt="Ícone" src={item.icon}/>
+          <MenuItemDescription openMenu={openMenu}>{item.description}</MenuItemDescription>
+        </MenuItem>
       ))}
     </MenuContainer>
   )
